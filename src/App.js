6395c6c1826643cd293464214128bb5d6a138e8d
rupal-gt <rupal.kataria@groundtruth.com>
2020-02-11 15:06:14 +0530

    tect changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World  I am Rupal Kataria
+          Hello World  I am Rupal Kataria.
           We are here to make changes
         </p>
         <a
